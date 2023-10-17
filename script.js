4e14cd0042c31c7d02be35a4850061b5533bd3d3
kritan <nepalkritan47@gmail.com>
2023-10-17 22:59:51 +0545

    keeping everything simple for html part

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
-const Container = document.getElementById("container");
-
-function createGrid() {
-    for (let i = 0; i < 16 * 16; i++) {
+function createGrid( min , max) {
+    //function to create a grid of squares.
+    for (let i = 0; i < min * max; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         Container.appendChild(square);
     }
 }
-createGrid();
+const rangeInput = document.getElementById("rangeInput");
+const output = document.getElementById("output");
+
+rangeInput.addEventListener("input", function() {
+    const value = this.value;
+    const result = `${value}x${value}`;
+    output.textContent = result;
+});
